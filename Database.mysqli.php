7be9d36d4892889c33786b3be729b904741b4b04
electrikmilk <brandonjordan124@gmail.com>
2022-02-01 23:21:44 -0500

    Finish mysqli wrapper class

--- Database.mysqli.php
+++ Database.mysqli.php
@@ -2,14 +2,25 @@
 
 class Database
 {
-	private $connection;
-	private $db;
-	private $error;
+	private mysqli $connection;
+	private string $error;
 
-	public function __construct($db)
+	/**
+	 * @throws ErrorException
+	 */
+	public function __construct(string $db)
 	{
-		$this->db = $db;
-		$this->connection = mysqli_connect('host', 'user', 'pass', $this->db);
+		try {
+			$this->connection = mysqli_connect('hostname', 'username', 'password', $db);
+			if (!$this->connection) throw new RuntimeException("Database connection failed.");
+		} catch (Exception $e) {
+			echo 'Caught exception: ' . $e->getMessage();
+		}
+	}
+
+	public function error()
+	{
+		return $this->error ?? false;
 	}
 
 	private function create($table, $fields)
@@ -24,12 +35,12 @@ class Database
 			}
 		}
 		$values = implode("`,`", $values);
-		$query = mysqli_query($this->connection, "insert into $db.$table ($cols) values (`$values`)");
-		if(!$query) {
-			$this->$error = mysqli_error($connect);
+		$query = mysqli_query($this->connection, "insert into $table ($cols) values (`$values`)");
+		if (!$query) {
+			$this->error = "Database Error: " . mysqli_error($this->connection);
 			return false;
 		}
-		return mysqli_insert_id();
+		return mysqli_insert_id($this->connection);
 	}
 
 	private function read($table, $where, $order = null, $limit = null)
@@ -42,8 +53,17 @@ class Database
 			}
 		}
 		if ($order) {
-			if (count($order) === 2 || ($order === 1 && $order[0] === "RAND()")) {
-				$where = implode(" ", $where);
+			if (count($order) === 2 || (count($order) === 1 && $order[0] === "RAND()")) {
+				$order = implode(" ", $order);
+				if ($order[1]) {
+					try {
+						if ($order[1] !== "asc" || $order[1] !== "desc") {
+							throw new RuntimeException("Tried to sort by $order[1].");
+						}
+					} catch (Exception $e) {
+						echo 'Caught exception: ' . $e->getMessage();
+					}
+				}
 			} else {
 				return false;
 			}
@@ -51,16 +71,49 @@ class Database
 		if ($limit) {
 			$limit = "limit $limit";
 		}
-		$query = mysqli_query($this->connection, "select * from $db.$table where $where $order $limit");
+		$query = mysqli_query($this->connection, "select * from $table where $where $order $limit");
 		if (mysqli_num_rows($query) === 0) {
 			return false;
 		}
 		return mysqli_fetch_array($query);
 	}
 
-	private function modify($action, $table, $fields, $where)
+	private function modify($action, $table, $fields, $where): bool
 	{
-		// todo
+		$update_fields = [];
+		if (!count($where) || count($where) !== 3) {
+			return false;
+		}
+		if ($action === "update" && !count($fields)) {
+			return false;
+		}
+		if ($action === "delete") {
+			$action = "delete from";
+		}
+		if (count($fields)) {
+			foreach ($fields as $column => $value) {
+				if (!$value) {
+					$value = "NULL";
+				} else {
+					$value = "'" . mysqli_real_escape_string($this->connection, $value) . "'";
+				}
+				$update_fields[] = "$column = $value";
+			}
+			$update_fields = "set " . implode(", ", $update_fields);
+		}
+		if (count($where) === 3) {
+			$where = implode(" ", $where);
+		} else {
+			return false;
+		}
+		if (is_array($update_fields)) {
+			unset($update_fields);
+		}
+		$query = mysqli_query($this->connection, "$action $table $update_fields where $where");
+		if (!$query) {
+			return false;
+		}
+		return true;
 	}
 
 	public function get(string $table, $where, ?array $order, ?int $limit)
@@ -68,7 +121,7 @@ class Database
 		return $this->read($table, $where);
 	}
 
-	public function update(string $table, array $fields, $where)
+	public function update(string $table, array $fields, array $where): bool
 	{
 		return $this->modify("update", $table, $fields, $where);
 	}
@@ -78,7 +131,7 @@ class Database
 		return $this->create($table, $fields);
 	}
 
-	public function delete(string $table, $where)
+	public function delete(string $table, array $where): bool
 	{
 		return $this->modify("delete", $table, false, $where);
 	}
